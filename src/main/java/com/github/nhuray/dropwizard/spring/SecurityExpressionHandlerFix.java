5544f2c5a70a34a7fb0a230ebe304b46ac232364
Guido Kämper <gk@exclamation.de>
2014-02-07 00:10:45 +0100

    Add Security Handler

---
+++ src/main/java/com/github/nhuray/dropwizard/spring/SecurityExpressionHandlerFix.java
@@ -0,0 +1,32 @@
+package com.github.nhuray.dropwizard.spring;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericApplicationContext;
+import org.springframework.security.access.expression.SecurityExpressionHandler;
+import org.springframework.web.context.support.WebApplicationContextUtils;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import java.util.Map;
+
+public class SecurityExpressionHandlerFix implements ServletContextListener {
+
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        ApplicationContext webApplicationContext = WebApplicationContextUtils
+                .getRequiredWebApplicationContext(sce.getServletContext());
+        ApplicationContext parentContext = webApplicationContext.getParent();
+        String[] beanNamesForType = parentContext.getBeanNamesForType(SecurityExpressionHandler.class);
+        Map<String, SecurityExpressionHandler> handlers = parentContext
+                .getBeansOfType(SecurityExpressionHandler.class);
+
+        for (String name : beanNamesForType) {
+            ((GenericApplicationContext)webApplicationContext).getBeanFactory().registerSingleton(name, parentContext.getBean(name));
+        }
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+
+    }
+}
